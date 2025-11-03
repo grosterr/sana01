05438cc6ba49c5fd930f978ca06d876b0e60f49d
Dima <ipz252_gdo@student.ztu.edu.ua>
2025-11-03 15:23:56 +0200

    1

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = UTF8Encoding.UTF8;
+double a, b, c, d, x, y, z, r;
+Console.WriteLine("Введіть a: " );
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть b: " );
+b = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть c: " );
+c = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть d: " );
+d = double.Parse(Console.ReadLine());
+x = (a+2*b-c+d)/ (c * d) + (a + b) / (c - d) - (Math.Pow(a,2)) / (Math.Pow(b,2));
+y = (5 * (a + b) * (c - d)) / ((1.0/2.0)*c) + Math.Pow(d, 2) * (Math.Pow(a,2) - Math.Pow(b, 2)) / (b-a);
+z = (Math.Pow((Math.Pow(x, 2)-2*x), 3)-4*(Math.Pow(x, 4)+1)*(1-b)) / (5*a+3*b);
+r = ((1.0/2.0)*a + (3.0/4.0)*b-(7.0/5.0))/(3*c+1)+(1/(a-c));
+Console.WriteLine($"X = {x}, Y = {y}, Z = {z} R = {r}");
