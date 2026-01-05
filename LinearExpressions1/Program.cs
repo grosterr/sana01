d621db757afdf910aba047b55978b519eb38b7fa
Dima <ipz252_gdo@student.ztu.edu.ua>
2026-01-05 15:47:23 +0200

    fixed

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -12,6 +12,6 @@ Console.WriteLine("Введіть d: " );
 d = double.Parse(Console.ReadLine());
 x = (a+2*b-c+d)/ (c * d) + (a + b) / (c - d) - (Math.Pow(a,2)) / (Math.Pow(b,2));
 y = (5 * (a + b) * (c - d)) / ((1.0/2.0)*c) + Math.Pow(d, 2) * (Math.Pow(a,2) - Math.Pow(b, 2)) / (b-a);
-z = (Math.Pow((Math.Pow(x, 2)-2*x), 3)-4*(Math.Pow(x, 4)+1)*(1-b)) / (5*a+3*b);
+z = (Math.Pow((Math.Pow(x, 2) - 2 * x), 3)-4*(Math.Pow(x, 4)+1)*(1-b))/(5*a+3*b);
 r = ((1.0/2.0)*a + (3.0/4.0)*b-(7.0/5.0))/(3*c+1)+(1/(a-c));
 Console.WriteLine($"X = {x}, Y = {y}, Z = {z} R = {r}");
--- LinearExpressions2/Program.cs
+++ LinearExpressions2/Program.cs
@@ -16,4 +16,5 @@ a = double.Parse(Console.ReadLine());
 z1 = ((m-1)*Math.Sqrt(m)-(n-1)*Math.Sqrt(n)) / ((Math.Sqrt(Math.Pow(m, 3)*n)) + n*m+ Math.Pow(m, 2)-m);
 z2 = (Math.Sqrt(m) - Math.Sqrt(n))/m;
 
-y = 2.4*(Math.Abs(Math.Pow(x, 2)+b)/a)+(a-b)*(Math.Pow(Math.Sin(a-b), 2))+Math.Pow(10,-2)*(x-b);
+y = 2.4 * Math.Abs((Math.Pow(x, 2)+b)/a) + (a-b) * Math.Pow(Math.Sin(a-b), 2) + Math.Pow(10, -2) * (x-b);
+Console.WriteLine($"Z1 = {z1}, Z2 = {z2}, Y = {y}");
